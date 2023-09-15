7b9e71c76695abf4b26e9a461afe7e8956a0101d
developer <mahlenko-weblive@yandex.ru>
2023-09-15 23:38:22 +0300

    Add Doctrine and setup Docker container for Postgres

---
+++ src/Shared/Infrastructure/Database/Migrations/Version20230915203325.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Shared\Infrastructure\Database\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230915203325 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE users_user (ulid VARCHAR(20) NOT NULL, email VARCHAR(50) NOT NULL, password VARCHAR(50) NOT NULL, created_at DATE NOT NULL, PRIMARY KEY(ulid))');
+        $this->addSql('COMMENT ON COLUMN users_user.created_at IS \'(DC2Type:date_immutable)\'');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP TABLE users_user');
+    }
+}
